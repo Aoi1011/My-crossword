ebaebe45abb424cf5e68793fbc2cfafff159dd2e
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-20 07:13:11 +0900

    feat: start implementing FieldElement

---
+++ finite-fields/src/lib.rs
@@ -0,0 +1,42 @@
+pub struct FieldElement {
+    num: u32,
+    prime: u32,
+}
+
+impl FieldElement {
+    pub fn new(num: u32, prime: u32) -> Self {
+        if num >= prime {
+            panic!("Num {} not in field range 0 to {}", num, prime - 1);
+        }
+        Self { num, prime }
+    }
+
+    pub fn repr(&self) {
+        println!("FieldElement_{}({})", self.prime, self.num);
+    }
+
+    pub fn eq(&self, other: Option<FieldElement>) -> bool {
+        if other.is_none() {
+            return false;
+        }
+
+        let other = other.unwrap();
+        self.num == other.num && self.prime == other.prime
+    }
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fieldelement_eq() {
+        let element = FieldElement::new(7, 13);
+        let other = FieldElement::new(6, 13);
+        assert!(!element.eq(Some(other)));
+    }
+}
