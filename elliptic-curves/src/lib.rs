24274af760620590cc132e64a8ff86028bc7d65b
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-25 06:45:42 +0900

    feat: implement p1 == p2

--- elliptic-curves/src/lib.rs
+++ elliptic-curves/src/lib.rs
@@ -32,24 +32,33 @@ impl Point {
         }
 
         match (self.x, other.x) {
-            (Some(self_x), Some(other_x)) if self_x == other_x && self.y != other.y => {
-                return Self {
-                    x: None,
-                    y: None,
-                    a: self.a,
-                    b: self.b,
-                }
-            }
+            (Some(self_x), Some(other_x)) if self_x == other_x && self.y != other.y => Self {
+                x: None,
+                y: None,
+                a: self.a,
+                b: self.b,
+            },
             (Some(self_x), Some(other_x)) if self_x != other_x => {
                 let s = (other.y.unwrap() - self.y.unwrap()) / (other.x.unwrap() - self.x.unwrap());
                 let x3 = s.pow(2) - self.x.unwrap() - other.x.unwrap();
                 let y3 = s * (self.x.unwrap() - x3) - self.y.unwrap();
-                return Self {
+                Self {
                     x: Some(x3),
                     y: Some(y3),
                     a: self.a,
                     b: self.b,
-                };
+                }
+            }
+            (Some(self_x), Some(other_x)) if self_x == other_x && self.y == other.y => {
+                let s = (3 * self_x.pow(2) + self.a) / (2 * self.y.unwrap());
+                let x3 = s.pow(2) - 2 * self_x;
+                let y3 = s * (self_x - x3) - self.y.unwrap();
+                Self {
+                    x: Some(x3),
+                    y: Some(y3),
+                    a: self.a,
+                    b: self.b,
+                }
             }
             (Some(_), None) => return *self,
             (None, Some(_)) => return other,
@@ -83,9 +92,17 @@ mod tests {
 
         assert_eq!(point1.add(point2), inf);
 
+        // x1 != x2
         point1 = Point::new(Some(2), Some(5), 5, 7);
         point2 = Point::new(Some(-1), Some(-1), 5, 7);
         let point3 = Point::new(Some(3), Some(-7), 5, 7);
+
+        assert_eq!(point1.add(point2), point3);
+
+        // p1 == p2
+        point1 = Point::new(Some(-1), Some(-1), 5, 7);
+        point2 = Point::new(Some(-1), Some(-1), 5, 7);
+        let point3 = Point::new(Some(18), Some(77), 5, 7);
 
         assert_eq!(point1.add(point2), point3);
     }
