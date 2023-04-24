7b1ff2fb470f57314ec24501d2877753e396d34f
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-25 08:26:26 +0900

    test: add new test for exercise 1

--- elliptic-curve-cryptography/src/lib.rs
+++ elliptic-curve-cryptography/src/lib.rs
@@ -7,8 +7,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_exercise1() {
+        let prime = 223;
+
+        let vec: Vec<(i32, i32)> = vec![(192, 105), (17, 56), (200, 119), (1, 193), (42, 99)];
+
+        for point in vec {
+            let x = point.0.pow(2) % prime;
+            let y = (point.1.pow(3) + 7) % prime;
+
+            println!("{} Point ({}, {})", x == y,  point.0, point.1);
+        }
     }
 }
