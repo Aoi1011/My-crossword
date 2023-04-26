a40b864a8fad4ba319cd1c1ace1e6f7244128450
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-27 07:55:31 +0900

    test: check point1 exist on curve

--- elliptic-curve-cryptography/src/lib.rs
+++ elliptic-curve-cryptography/src/lib.rs
@@ -5,6 +5,7 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use elliptic_curves::Point;
     use finite_fields::FieldElement;
 
     #[test]
@@ -36,6 +37,17 @@ mod tests {
         let a = FieldElement::new(0, prime);
         let b = FieldElement::new(7, prime);
 
+        let field_1 = FieldElement::new(170, prime);
+        let field_2 = FieldElement::new(142, prime);
+        let field_3 = FieldElement::new(60, prime);
+        let field_4 = FieldElement::new(139, prime);
+        let field_5 = FieldElement::new(220, prime);
+        let field_6 = FieldElement::new(181, prime);
 
+        let point_1 = Point::new(Some(field_1), Some(field_2), a, b);
+        let point_2 = Point::new(Some(field_3), Some(field_4), a, b);
+        let point_3 = Point::new(Some(field_5), Some(field_6), a, b);
+
+        assert_eq!(point_1.add(point_2), point_3);
     }
 }
