80cf6a16bd98012b4ee952a0b033fd80c6b0d8be
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-27 07:39:38 +0900

    fix: change i32 -> FieldElement

--- elliptic-curves/src/lib.rs
+++ elliptic-curves/src/lib.rs
@@ -4,26 +4,20 @@ use finite_fields::FieldElement;
 pub struct Point {
     pub x: Option<FieldElement>,
     pub y: Option<FieldElement>,
-    pub a: i32,
-    pub b: i32,
+    pub a: FieldElement,
+    pub b: FieldElement,
 }
 
 impl Point {
     // x.is_none() && y.is_none() -> the point at infinity
-    pub fn new(x: Option<FieldElement>, y: Option<FieldElement>, a: i32, b: i32) -> Self {
+    pub fn new(
+        x: Option<FieldElement>,
+        y: Option<FieldElement>,
+        a: FieldElement,
+        b: FieldElement,
+    ) -> Self {
         if let (Some(x_field), Some(y_field)) = (x, y) {
-            if y_field.pow(2)
-                != x_field
-                    .pow(3)
-                    .add(Some(x_field.mul(Some(FieldElement {
-                        num: a,
-                        prime: x_field.prime,
-                    }))))
-                    .add(Some(FieldElement {
-                        num: b,
-                        prime: x_field.prime,
-                    }))
-            {
+            if y_field.pow(2) != x_field.pow(3).add(Some(a.mul(Some(x_field)))).add(Some(b)) {
                 panic!("({:?}, {:?}) is not on the curve", x_field, y_field);
             }
         }
@@ -52,13 +46,16 @@ impl Point {
                 b: self.b,
             },
             (Some(self_x), Some(other_x)) if self_x != other_x => {
+                // s = (y2 - y1) / (x2 - x1)
                 let s = other
                     .y
                     .unwrap()
-                    .sub(Some(self.y.unwrap()))
-                    .true_dev(Some(other.x.unwrap().sub(Some(self.x.unwrap()))));
-                let x3 = s.pow(2) - self.x.unwrap() - other.x.unwrap();
-                let y3 = s * (self.x.unwrap() - x3) - self.y.unwrap();
+                    .sub(self.y)
+                    .true_dev(Some(other.x.unwrap().sub(self.x)));
+                // x3 = s ^ 2 - x1 - x2
+                let x3 = s.pow(2).sub(self.x).sub(other.x);
+                // y3 = s(x1 - x3) - y1
+                let y3 = s.mul(Some(self_x.sub(Some(x3)))).sub(self.y);
                 Self {
                     x: Some(x3),
                     y: Some(y3),
@@ -67,9 +64,15 @@ impl Point {
                 }
             }
             (Some(self_x), Some(other_x)) if self_x == other_x && self.y == other.y => {
-                let s = (3 * self_x.pow(2) + self.a) / (2 * self.y.unwrap());
-                let x3 = s.pow(2) - 2 * self_x;
-                let y3 = s * (self_x - x3) - self.y.unwrap();
+                let s = FieldElement::new(3, self_x.prime)
+                    .mul(Some(self_x.pow(2)))
+                    .add(Some(self.a))
+                    .true_dev(Some(FieldElement::new(2, self_x.prime).mul(self.y)));
+                let x3 = s
+                    .pow(2)
+                    .sub(Some(FieldElement::new(2, self_x.prime).mul(Some(self_x))));
+                let y3 = s.mul(Some(self_x.sub(Some(x3)))).sub(self.y);
+
                 Self {
                     x: Some(x3),
                     y: Some(y3),
@@ -91,35 +94,47 @@ impl Point {
 
 #[cfg(test)]
 mod tests {
+    use finite_fields::FieldElement;
+
     use crate::Point;
 
     #[test]
     fn test_equal() {
-        let point1 = Point::new(Some(-1), Some(-1), 5, 7);
-        let point2 = Point::new(Some(-1), Some(-1), 5, 7);
+        let field_element1 = FieldElement::new(-1, 27);
+        let field_element2 = FieldElement::new(-1, 27);
+        let a = FieldElement::new(5, 27);
+        let b = FieldElement::new(7, 27);
+        let point1 = Point::new(Some(field_element1), Some(field_element1), a, b);
+        let point2 = Point::new(Some(field_element2), Some(field_element2), a, b);
 
         assert!(point1.equal(Some(point2)));
     }
 
     #[test]
     fn test_add() {
-        let mut point1 = Point::new(Some(-1), Some(-1), 5, 7);
-        let mut point2 = Point::new(Some(-1), Some(1), 5, 7);
-        let inf = Point::new(None, None, 5, 7);
+        let field_element1 = FieldElement::new(-1, 27);
+        let field_element2 = FieldElement::new(1, 27);
+        let a = FieldElement::new(5, 27);
+        let b = FieldElement::new(7, 27);
+        let point1 = Point::new(Some(field_element1), Some(field_element1), a, b);
+        let point2 = Point::new(Some(field_element1), Some(field_element2), a, b);
+        let inf = Point::new(None, None, a, b);
 
         assert_eq!(point1.add(point2), inf);
 
         // x1 != x2
-        point1 = Point::new(Some(2), Some(5), 5, 7);
-        point2 = Point::new(Some(-1), Some(-1), 5, 7);
-        let point3 = Point::new(Some(3), Some(-7), 5, 7);
-
-        assert_eq!(point1.add(point2), point3);
-
-        // p1 == p2
-        point1 = Point::new(Some(-1), Some(-1), 5, 7);
-        point2 = Point::new(Some(-1), Some(-1), 5, 7);
-        let point3 = Point::new(Some(18), Some(77), 5, 7);
+        let prime = 223;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+        let field_element1 = FieldElement::new(170, prime);
+        let field_element2 = FieldElement::new(142, prime);
+        let field_element3 = FieldElement::new(60, prime);
+        let field_element4 = FieldElement::new(139, prime);
+        let field_element5 = FieldElement::new(220, prime);
+        let field_element6 = FieldElement::new(181, prime);
+        let point1 = Point::new(Some(field_element1), Some(field_element2), a, b);
+        let point2 = Point::new(Some(field_element3), Some(field_element4), a, b);
+        let point3 = Point::new(Some(field_element5), Some(field_element6), a, b);
 
         assert_eq!(point1.add(point2), point3);
     }
