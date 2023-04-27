2a42b93be3697de3a8b1ec83ae5dd7469ed88e26
Aoi1011 <ht.so.ik0102@gmail.com>
2023-04-28 06:50:54 +0900

    test: add new test case for scalar calculation

--- elliptic-curve-cryptography/src/lib.rs
+++ elliptic-curve-cryptography/src/lib.rs
@@ -76,4 +76,55 @@ mod tests {
 
         assert_eq!(point_1.add(point_2), point_3);
     }
+
+    #[test]
+    fn test_on_curve_scalar() {
+        let prime = 223;
+        let a = FieldElement::new(0, prime);
+        let b = FieldElement::new(7, prime);
+
+        let mut field_1 = FieldElement::new(192, prime);
+        let mut field_2 = FieldElement::new(105, prime);
+        let mut field_3 = FieldElement::new(49, prime);
+        let mut field_4 = FieldElement::new(71, prime);
+        // let mut field_5 = FieldElement::new(220, prime);
+        // let mut field_6 = FieldElement::new(181, prime);
+
+        let mut point_1 = Point::new(Some(field_1), Some(field_2), a, b);
+        let mut point_2 = Point::new(Some(field_3), Some(field_4), a, b);
+        // let mut point_3 = Point::new(Some(field_5), Some(field_6), a, b);
+
+        assert_eq!(point_1.add(point_1), point_2);
+
+        field_1 = FieldElement::new(143, prime);
+        field_2 = FieldElement::new(98, prime);
+        field_3 = FieldElement::new(64, prime);
+        field_4 = FieldElement::new(168, prime);
+        // field_5 = FieldElement::new(215, prime);
+        // field_6 = FieldElement::new(68, prime);
+
+        point_1 = Point::new(Some(field_1), Some(field_2), a, b);
+        point_2 = Point::new(Some(field_3), Some(field_4), a, b);
+        // point_3 = Point::new(Some(field_5), Some(field_6), a, b);
+
+        assert_eq!(point_1.add(point_1), point_2);
+
+        field_1 = FieldElement::new(47, prime);
+        field_2 = FieldElement::new(71, prime);
+        field_3 = FieldElement::new(36, prime);
+        field_4 = FieldElement::new(111, prime);
+        // field_5 = FieldElement::new(47, prime);
+        // field_6 = FieldElement::new(71, prime);
+
+        point_1 = Point::new(Some(field_1), Some(field_2), a, b);
+        point_2 = Point::new(Some(field_3), Some(field_4), a, b);
+        // point_3 = Point::new(Some(field_5), Some(field_6), a, b);
+
+        assert_eq!(point_1.add(point_1), point_2);
+
+        field_3 = FieldElement::new(194, prime);
+        field_4 = FieldElement::new(51, prime);
+        point_2 = Point::new(Some(field_3), Some(field_4), a, b);
+        assert_eq!(point_1.add(point_1).add(point_1).add(point_1), point_2);
+    }
 }
