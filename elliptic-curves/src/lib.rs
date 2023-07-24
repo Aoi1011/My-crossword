3c18404ccc26b5b4a9de35aead92c9f037116bc0
Aoi1011 <ht.so.ik0102@gmail.com>
2023-07-24 09:26:45 +0900

    feat: import num_bigint

--- elliptic-curves/src/lib.rs
+++ elliptic-curves/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{ops::Add, str::FromStr};
 
-use ethers_core::types::I256;
+use ethereum_types::U256;
 use finite_fields::FieldElement;
 
 const A: &str = "0000000000000000000000000000000000000000000000000000000000000000";
@@ -24,19 +24,21 @@ impl Point {
         b: Option<FieldElement>,
     ) -> Self {
         let a = if a.is_none() {
-            FieldElement::new(I256::from_str(A).unwrap(), None)
+            FieldElement::new(U256::from_str(A).unwrap(), None)
         } else {
             a.unwrap()
         };
 
         let b = if b.is_none() {
-            FieldElement::new(I256::from_str(B).unwrap(), None)
+            FieldElement::new(U256::from_str(B).unwrap(), None)
         } else {
             b.unwrap()
         };
 
         if let (Some(x_field), Some(y_field)) = (x.clone(), y.clone()) {
-            if y_field.pow(2) != x_field.pow(3).add(&a.mul(&x_field)).add(&b) {
+            if y_field.pow(U256::from(2))
+                != x_field.pow(U256::from(3)).add(&a.mul(&x_field)).add(&b)
+            {
                 panic!("({:?}, {:?}) is not on the curve", x_field, y_field);
             }
         }
@@ -97,7 +99,10 @@ impl Add for Point {
                     .sub(&self.y.clone().unwrap())
                     .true_div(&other_x.sub(&self.x.clone().unwrap()));
                 // x3 = s ^ 2 - x1 - x2
-                let x3 = s.pow(2).sub(&self.x.unwrap()).sub(&rhs.x.clone().unwrap());
+                let x3 = s
+                    .pow(U256::from(2))
+                    .sub(&self.x.unwrap())
+                    .sub(&rhs.x.clone().unwrap());
                 // y3 = s(x1 - x3) - y1
                 let y3 = s.mul(&self_x.sub(&x3)).sub(&self.y.unwrap());
                 Self {
@@ -110,16 +115,16 @@ impl Add for Point {
             (Some(self_x), Some(other_x)) if self_x == other_x && self.y == rhs.y => {
                 let x_prime = self_x.clone().prime;
                 // s = (3x1 ^ 2 + a) / (2y1)
-                let s = FieldElement::new(I256::from_str("3").unwrap(), Some(x_prime.clone()))
-                    .mul(&self_x.pow(2))
+                let s = FieldElement::new(U256::from_str("3").unwrap(), Some(x_prime.clone()))
+                    .mul(&self_x.pow(U256::from(2)))
                     .add(&self.a)
                     .true_div(
-                        &FieldElement::new(I256::from_str("2").unwrap(), Some(x_prime.clone()))
+                        &FieldElement::new(U256::from_str("2").unwrap(), Some(x_prime.clone()))
                             .mul(&self.y.clone().unwrap()),
                     );
                 // x3 = s ^ 2 - 2x1
-                let x3 = s.pow(2).sub(
-                    &FieldElement::new(I256::from_str("2").unwrap(), Some(x_prime.clone()))
+                let x3 = s.pow(U256::from(2)).sub(
+                    &FieldElement::new(U256::from_str("2").unwrap(), Some(x_prime.clone()))
                         .mul(&self_x),
                 );
                 // y3 = s(x1 - x3) - y1
@@ -148,26 +153,27 @@ impl Add for Point {
 mod tests {
     use std::str::FromStr;
 
-    use ethers_core::types::{I256, H512, U512, U256};
+    use ethereum_types::{U256, U512};
     use finite_fields::FieldElement;
-    use revm_primitives::B256;
+    use num_bigint::BigUint;
+    use num_traits::Num;
 
     use crate::Point;
 
-    macro_rules! i256 {
+    macro_rules! u256 {
         ($val: expr) => {
-            I256::from($val)
+            U256::from($val)
         };
     }
 
     #[test]
     fn test_equal() {
-        let prime = Some(i256!(27));
+        let prime = Some(u256!(27));
 
-        let field_element1 = FieldElement::new(i256!(-1), prime.clone());
-        let field_element2 = FieldElement::new(i256!(-1), prime.clone());
-        let a = FieldElement::new(i256!(5), prime.clone());
-        let b = FieldElement::new(i256!(7), prime.clone());
+        let field_element1 = FieldElement::new(u256!(-1), prime.clone());
+        let field_element2 = FieldElement::new(u256!(-1), prime.clone());
+        let a = FieldElement::new(u256!(5), prime.clone());
+        let b = FieldElement::new(u256!(7), prime.clone());
         let point1 = Point::new(
             Some(field_element1.clone()),
             Some(field_element1),
@@ -242,30 +248,22 @@ mod tests {
         let x = "0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
         let y = "0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8";
 
-        let field_x = FieldElement::new(I256::from_str(x).unwrap(), None);
-        let field_y = FieldElement::new(I256::from_str(y).unwrap(), None);
-        let point_g = Point::new(Some(field_x.clone()), Some(field_y.clone()), None, None);
+        let gx = BigUint::from_str_radix(x, 16);
+        let gy = BigUint::from_str_radix(y, 16);
+        // let field_y = FieldElement::new(U256::from_str(y).unwrap(), None);
+        // let point_g = Point::new(Some(field_x.clone()), Some(field_y.clone()), None, None);
 
-        let n = I256::from_str(N).unwrap();
-
-        let temp_x = FieldElement::new(field_x.num * n.clone() % field_x.prime, None);
-        let temp_y = FieldElement::new(field_y.num * n % field_y.prime, None);
-        let temp = Point::new(Some(temp_x), Some(temp_y), None, None);
-
-        assert!(point_g.x == temp.x && point_g.y != temp.y);
-    }
-
-    #[test]
-    fn test_h512() {
-        let x = "0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798";
-        let y = "0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8";
-
-
-        let b256_x = U256::from_str(x).unwrap();
-        let b256_y = U256::from_str(y).unwrap();
-
-        let c = b256_x + b256_y;
-
+        let n = BigUint::from_str_radix(N, 16).unwrap();
         
+        let c = n.clone() + n.clone();
+
+        // let temp_x = FieldElement::new(
+        //     field_x.num.checked_mul(n.clone()).unwrap() % field_x.prime,
+        //     None,
+        // );
+        // let temp_y = FieldElement::new(field_y.num * n % field_y.prime, None);
+        // let temp = Point::new(Some(temp_x), Some(temp_y), None, None);
+
+        // assert!(point_g.x == temp.x && point_g.y != temp.y);
     }
 }
