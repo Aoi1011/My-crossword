f7c145da0a4dc4a862fd4d0f75f0af7281e40955
aoikurokawa <ht.so.ik0102@gmail.com>
2023-08-13 13:53:33 +0900

    feat: implementing serialize function

--- script/src/lib.rs
+++ script/src/lib.rs
@@ -1,15 +1,22 @@
-use std::io::{self, Read};
+use std::{
+    io::{self, Read},
+    ops::Add,
+};
 
 use bitcoin::consensus::Decodable;
-use elliptic_curves::helper::{little_endian_bytes_to_u64, read_varint};
+use elliptic_curves::helper::{
+    encode_varint, int_to_little_endian, little_endian_bytes_to_u64, read_varint,
+};
 
 pub mod op;
 
+#[derive(Debug, Clone)]
 pub enum Cmd {
     OpCode(u8),
     PushData(Vec<u8>),
 }
 
+#[derive(Debug, Clone)]
 pub struct Script {
     pub cmds: Vec<Cmd>,
 }
@@ -67,6 +74,58 @@ impl Script {
         }
 
         Ok(Self { cmds })
+    }
+
+    pub fn raw_serialize(&self) -> Vec<u8> {
+        let mut res = Vec::new();
+
+        for cmd in &self.cmds {
+            match cmd {
+                Cmd::OpCode(op_code) => {
+                    res.append(&mut int_to_little_endian(*op_code as u128, 1));
+                }
+
+                Cmd::PushData(ref data) => {
+                    let length = data.len();
+                    if length < 75 {
+                        res.append(&mut int_to_little_endian(length as u128, 1));
+                    } else if length > 75 && length < 0x100 {
+                        res.append(&mut int_to_little_endian(76_u128, 1));
+                        res.append(&mut int_to_little_endian(length as u128, 1));
+                    } else if length >= 0x100 && length <= 520 {
+                        res.append(&mut int_to_little_endian(77_u128, 1));
+                        res.append(&mut int_to_little_endian(length as u128, 2));
+                    } else {
+                        panic!("too long an cmd")
+                    }
+
+                    res.append(&mut data.clone());
+                }
+            }
+        }
+
+        res
+    }
+
+    pub fn serialize(&self) -> Vec<u8> {
+        let mut result = self.raw_serialize();
+        let total = result.len();
+
+        let mut encoded = encode_varint(total as u128);
+        encoded.append(&mut result);
+        encoded
+    }
+}
+
+impl Add for Script {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut res = Vec::new();
+        res.append(&mut self.cmds.clone());
+        res.append(&mut rhs.cmds.clone());
+
+        Self { cmds: res }
     }
 }
 
