c289124f13e8c6d2e0b7f13a60738dcf3a8f2b13
aoikurokawa <ht.so.ik0102@gmail.com>
2023-08-19 18:37:05 +0900

    feat: implementing populate_tree function

--- merkleblock/src/lib.rs
+++ merkleblock/src/lib.rs
@@ -1,15 +1,17 @@
+use elliptic_curves::helper::merkle_parent;
+
 #[derive(Debug)]
 pub struct MerkleTree {
     total: u16,
     max_depth: u32,
     nodes: Vec<Vec<Option<Vec<u8>>>>,
-    current_depth: u16,
-    current_index: u16,
+    current_depth: u32,
+    current_index: u32,
 }
 
 impl MerkleTree {
     pub fn new(total: u16) -> Self {
-        let max_depth = (total as f64).log2().ceil() as u32;
+        let max_depth = (f64::log2(total as f64)).ceil() as u32;
 
         let mut nodes = Vec::new();
         for depth in 0..max_depth + 1 {
@@ -25,6 +27,86 @@ impl MerkleTree {
             nodes,
             current_depth: 0,
             current_index: 0,
+        }
+    }
+
+    pub fn up(&mut self) {
+        self.current_depth -= 1;
+        self.current_index /= 2;
+    }
+
+    pub fn left(&mut self) {
+        self.current_depth += 1;
+        self.current_index *= 2;
+    }
+
+    pub fn right(&mut self) {
+        self.current_depth += 1;
+        self.current_index = self.current_index * 2 + 1;
+    }
+
+    pub fn root(&self) -> &Option<Vec<u8>> {
+        &self.nodes[0][0]
+    }
+
+    pub fn set_current_node(&mut self, value: Option<Vec<u8>>) {
+        self.nodes[self.current_depth as usize][self.current_index as usize] = value;
+    }
+
+    pub fn get_current_node(&self) -> &Option<Vec<u8>> {
+        &self.nodes[self.current_depth as usize][self.current_index as usize]
+    }
+
+    pub fn get_left_node(&self) -> &Option<Vec<u8>> {
+        &self.nodes[(self.current_depth + 1) as usize][(self.current_index * 2) as usize]
+    }
+
+    pub fn get_right_node(&self) -> &Option<Vec<u8>> {
+        &self.nodes[(self.current_depth + 1) as usize][(self.current_index * 2 + 1) as usize]
+    }
+
+    pub fn is_leaf(&self) -> bool {
+        self.current_depth == self.max_depth
+    }
+
+    pub fn right_exists(&self) -> bool {
+        self.nodes[(self.current_depth + 1) as usize].len() > (self.current_index * 2 + 1) as usize
+    }
+
+    pub fn populate_tree(&mut self, flag_bits: &mut Vec<u8>, hashes: &mut Vec<Option<Vec<u8>>>) {
+        while self.root().is_none() {
+            if self.is_leaf() {
+                flag_bits.remove(0);
+                self.set_current_node(hashes.remove(0));
+                self.up();
+            } else {
+                let left_hash = self.get_left_node();
+                if left_hash.is_none() {
+                    if flag_bits.remove(0) == 0 {
+                        self.set_current_node(hashes.remove(0));
+                        self.up();
+                    } else {
+                        self.left();
+                    }
+                } else if self.right_exists() {
+                    let right_hash = self.get_right_node();
+                    if right_hash.is_none() {
+                        self.right();
+                    } else {
+                        self.set_current_node(Some(merkle_parent(
+                            &mut left_hash.clone(),
+                            &mut right_hash.clone(),
+                        )));
+                        self.up();
+                    }
+                } else {
+                    self.set_current_node(Some(merkle_parent(
+                        &mut left_hash.clone(),
+                        &mut left_hash.clone(),
+                    )));
+                    self.up();
+                }
+            }
         }
     }
 }
@@ -57,8 +139,8 @@ impl std::fmt::Display for MerkleTree {
 
 #[cfg(test)]
 mod tests {
-    use elliptic_curves::helper::merkle_parent_level;
-    use hex::FromHex;
+    use elliptic_curves::helper::{merkle_parent, merkle_parent_level};
+    use hex::{FromHex, ToHex};
 
     use super::*;
 
@@ -76,7 +158,7 @@ mod tests {
     }
 
     #[test]
-    fn test_print() {
+    fn test_display() {
         let hex_hashes = vec![
             "9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb",
             "5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b",
@@ -109,5 +191,94 @@ mod tests {
         tree.nodes[0] = merkle_parent_level(&mut tree.nodes[1]);
 
         println!("{}", tree);
+    }
+
+    #[test]
+    fn test_print() {
+        let hex_hashes = vec![
+            "9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb",
+            "5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b",
+            "82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05",
+            "507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308",
+            "a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330",
+            "bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add",
+            "ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836",
+            "457743861de496c429912558a106b810b0507975a49773228aa788df40730d41",
+            "7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a",
+            "b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9",
+            "9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab",
+            "b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638",
+            "b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263",
+            "c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800",
+            "c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2",
+            "f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e",
+        ];
+        let mut tree = MerkleTree::new(hex_hashes.len() as u16);
+        let hashes: Vec<Option<Vec<u8>>> = hex_hashes
+            .iter()
+            .map(|h| Some(Vec::from_hex(h).unwrap()))
+            .collect();
+
+        tree.nodes[4] = hashes;
+
+        while tree.root().is_none() {
+            if tree.is_leaf() {
+                tree.up();
+            } else {
+                let left_hash = tree.get_left_node();
+                let right_hash = tree.get_right_node();
+
+                if left_hash.is_none() {
+                    tree.left();
+                } else if right_hash.is_none() {
+                    tree.right();
+                } else {
+                    tree.set_current_node(Some(merkle_parent(
+                        &mut left_hash.clone(),
+                        &mut right_hash.clone(),
+                    )));
+                    tree.up();
+                }
+            }
+        }
+
+        println!("{}", tree);
+    }
+
+    #[test]
+    fn test_populate_tree_1() {
+        let hex_hashes = vec![
+            "9745f7173ef14ee4155722d1cbf13304339fd00d900b759c6f9d58579b5765fb",
+            "5573c8ede34936c29cdfdfe743f7f5fdfbd4f54ba0705259e62f39917065cb9b",
+            "82a02ecbb6623b4274dfcab82b336dc017a27136e08521091e443e62582e8f05",
+            "507ccae5ed9b340363a0e6d765af148be9cb1c8766ccc922f83e4ae681658308",
+            "a7a4aec28e7162e1e9ef33dfa30f0bc0526e6cf4b11a576f6c5de58593898330",
+            "bb6267664bd833fd9fc82582853ab144fece26b7a8a5bf328f8a059445b59add",
+            "ea6d7ac1ee77fbacee58fc717b990c4fcccf1b19af43103c090f601677fd8836",
+            "457743861de496c429912558a106b810b0507975a49773228aa788df40730d41",
+            "7688029288efc9e9a0011c960a6ed9e5466581abf3e3a6c26ee317461add619a",
+            "b1ae7f15836cb2286cdd4e2c37bf9bb7da0a2846d06867a429f654b2e7f383c9",
+            "9b74f89fa3f93e71ff2c241f32945d877281a6a50a6bf94adac002980aafe5ab",
+            "b3a92b5b255019bdaf754875633c2de9fec2ab03e6b8ce669d07cb5b18804638",
+            "b5c0b915312b9bdaedd2b86aa2d0f8feffc73a2d37668fd9010179261e25e263",
+            "c9d52c5cb1e557b92c84c52e7c4bfbce859408bedffc8a5560fd6e35e10b8800",
+            "c555bc5fc3bc096df0a0c9532f07640bfb76bfe4fc1ace214b8b228a1297a4c2",
+            "f9dbfafc3af3400954975da24eb325e326960a25b87fffe23eef3e7ed2fb610e",
+        ];
+        let mut tree = MerkleTree::new(hex_hashes.len() as u16);
+
+        let mut hashes: Vec<Option<Vec<u8>>> = hex_hashes
+            .iter()
+            .map(|h| Some(Vec::from_hex(h).unwrap()))
+            .collect();
+
+        tree.populate_tree(&mut vec![1; 31], &mut hashes);
+
+        let root = "597c4bafe3832b17cbbabe56f878f4fc2ad0f6a402cee7fa851a9cb205f87ed1";
+
+        assert_eq!(
+            tree.root().clone().unwrap().encode_hex::<String>(),
+            root.to_string()
+        );
     }
 }
